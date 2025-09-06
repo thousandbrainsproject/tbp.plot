776e01247b0945ff4ee4bb22bac3850d981b3b30
ramyamounir <44840015+ramyamounir@users.noreply.github.com>
2025-09-06 19:31:23 -0400

    chore: minor typo

--- tests/unit/pose_error_over_time_test.py
+++ tests/unit/pose_error_over_time_test.py
@@ -9,10 +9,10 @@
 
 import unittest
 
-from tbp.plot.plots.pose_error_over_time import main as interactive_plot
+from tbp.plot.plots.pose_error_over_time import main as plot
 
 
 class TestPoseErrorOverTime(unittest.TestCase):
     def test_exit_1_if_exp_path_does_not_exist(self):
-        exit_code = interactive_plot("nonexistent_path")
+        exit_code = plot("nonexistent_path")
         self.assertEqual(exit_code, 1)
