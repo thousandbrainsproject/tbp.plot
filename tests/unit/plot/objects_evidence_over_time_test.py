5157d13222833fb938a4343c4b0ebaf9f49e3d98
Ramy Mounir <44840015+ramyamounir@users.noreply.github.com>
2025-09-08 12:07:18 -0400

    feat: port objects_evidence_over_time plot (#5)

---
+++ tests/unit/plot/objects_evidence_over_time_test.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Thousand Brains Project
+#
+# Copyright may exist in Contributors' modifications
+# and/or contributions to the work.
+#
+# Use of this source code is governed by the MIT
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+import unittest
+
+from tbp.plot.plots.objects_evidence_over_time import main as plot
+
+
+class TestObjectsEvidenceOverTime(unittest.TestCase):
+    def test_exit_1_if_exp_path_does_not_exist(self):
+        exit_code = plot("nonexistent_path")
+        self.assertEqual(exit_code, 1)
